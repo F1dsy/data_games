232c2ee7c03cec79764f0a8b923eb237d2582d11
jsp566 <antonni9801@gmail.com>
2024-06-04 12:21:50 +0200

    Creating tables

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,30 @@ app.config["SECRET_KEY"] = "fc089b9218301ad987914c53481bff04"
 # db = "dbname='bank' user='postgres' host='127.0.0.1' password = 'UIS'"
 db = "dbname='flask_project' user='postgres' host='127.0.0.1' password = 'password123'"
 conn = psycopg2.connect(db)
+conn.autocommit = True
+
+# Creating tables:
+cursor = conn.cursor() 
+cursor.execute(open("app/schema.sql", "r").read())
+cursor.close()
+
+# Insert data into tables:
+#cursor = conn.cursor()
+
+insertUSER = "INSERT INTO Users(User_ID, Username, Password) VALUES (%d, %s, %s)"
+insertDATASET = "INSERT INTO Dataset(Dataset_ID, Dataset_Name) VALUES (%d, %s)"
+insertDATAROW = "INSERT INTO Datarow(Dataset_ID, Country, Value) VALUES (%d, %s, %f)"
+insertHighscore = "INSERT INTO Highscore(User_ID, Dataset_ID, Score) VALUES (%d, %d, %d)"
+insertSCOREProgress = "INSERT INTO ScoreProgress(User_ID, Dataset_ID, Score, Country1, Country2) VALUES (%d, %d, %d, %s, %s)"
+
+updateUSER = "UPDATE Users SET Username = %s, Password = %s WHERE User_ID = %d"
+updateHighscore = "UPDATE Highscore SET Score = %d WHERE User_ID = %d AND Dataset_ID = %d"
+updateSCOREProgress = "UPDATE ScoreProgress SET Score = %d, Country1 = %s, Country2 = %s WHERE User_ID = %d AND Dataset_ID = %d"
+
+deleteUSER = "DELETE FROM Users WHERE User_ID = %d"
+
+
+
 
 # bcrypt = Bcrypt(app)
 
