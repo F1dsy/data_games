5ae9230ca768cee589af7d503c02dd6b0e602c7f
jsp566 <antonni9801@gmail.com>
2024-06-04 12:59:23 +0200

    Insert data

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 import psycopg2
+from os import listdir
+import pandas as pd
 
 # from flask_bcrypt import Bcrypt
 # from flask_login import LoginManager
@@ -24,20 +26,37 @@ cursor.execute(open("app/schema.sql", "r").read())
 cursor.close()
 
 # Insert data into tables:
-#cursor = conn.cursor()
+Games = []
+filenames = listdir("app/data/games")
+for name in filenames:
+    if name.endswith(".csv"):
+        Games.append(name[:-4])
 
-insertUSER = "INSERT INTO Users(User_ID, Username, Password) VALUES (%d, %s, %s)"
-insertDATASET = "INSERT INTO Dataset(Dataset_ID, Dataset_Name) VALUES (%d, %s)"
-insertDATAROW = "INSERT INTO Datarow(Dataset_ID, Country, Value) VALUES (%d, %s, %f)"
-insertHighscore = "INSERT INTO Highscore(User_ID, Dataset_ID, Score) VALUES (%d, %d, %d)"
-insertSCOREProgress = "INSERT INTO ScoreProgress(User_ID, Dataset_ID, Score, Country1, Country2) VALUES (%d, %d, %d, %s, %s)"
+insertDATASET = "INSERT INTO Dataset(Dataset_ID, Dataset_Name) VALUES (%s, %s)"
+insertDATAROW = "INSERT INTO Datarow(Dataset_ID, Country, Value) VALUES (%s, %s, %s)"
 
-updateUSER = "UPDATE Users SET Username = %s, Password = %s WHERE User_ID = %d"
-updateHighscore = "UPDATE Highscore SET Score = %d WHERE User_ID = %d AND Dataset_ID = %d"
-updateSCOREProgress = "UPDATE ScoreProgress SET Score = %d, Country1 = %s, Country2 = %s WHERE User_ID = %d AND Dataset_ID = %d"
+cursor = conn.cursor()
+for i in range(len(Games)):
+    cursor.execute(insertDATASET, (i, Games[i]))
+    data = pd.read_csv("app/data/games/" + Games[i] + ".csv")
+    for j in range(len(data)):
+        cursor.execute(insertDATAROW, (i, data.iloc[j, 0], data.iloc[j, 1]))
+cursor.close()
 
-deleteUSER = "DELETE FROM Users WHERE User_ID = %d"
 
+insertUSER = "INSERT INTO Users(User_ID, Username, Password) VALUES (%s, %s, %s)"
+insertHighscore = "INSERT INTO Highscore(User_ID, Dataset_ID, Score) VALUES (%s, %s, %s)"
+insertSCOREProgress = "INSERT INTO ScoreProgress(User_ID, Dataset_ID, Score, Country1, Country2) VALUES (%s, %s, %s, %s, %s)"
+
+updateUSER = "UPDATE Users SET Username = %s, Password = %s WHERE User_ID = %s"
+updateHighscore = "UPDATE Highscore SET Score = %s WHERE User_ID = %s AND Dataset_ID = %s"
+updateSCOREProgress = "UPDATE ScoreProgress SET Score = %s, Country1 = %s, Country2 = %s WHERE User_ID = %s AND Dataset_ID = %s"
+
+deleteUSER = "DELETE FROM Users WHERE User_ID = %s"
+
+selectHighscore = "SELECT score FROM Highscore WHERE User_ID = %s AND Dataset_ID = %s"
+selectSCOREprogress = "SELECT score, Country1, Country2 FROM ScoreProgress WHERE User_ID = %s AND Dataset_ID = %s"
+selectTOP10 = "SELECT user_id, name, score FROM Highscore Natural join Users WHERE Dataset_ID = %s ORDER BY Score DESC LIMIT 10"
 
 
 
