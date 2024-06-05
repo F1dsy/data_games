c16cc203cf6406c62e2525a18f167da497e3992c
milehighprofit <100164539+milehighprofit@users.noreply.github.com>
2024-06-05 15:27:32 +0200

    added styling

--- app/__init__.py
+++ app/__init__.py
@@ -25,27 +25,6 @@ conn.autocommit = True
 
 GETDATA.create_tables(conn)
 
-# bcrypt = Bcrypt(app)
-# login_manager = LoginManager(app)
-# login_manager.login_view = "login"
-# login_manager.login_message_category = "info"
-
-# Check Configuration section for more details
-# SESSION_TYPE = 'filesystem'
-
-#roles = ["ingen", "employee", "customer"]
-#print(roles)
-#mysession = {"state": "initializing", "role": "Not assingned", "id": 0, "age": 202212}
-#print(mysession)
-
-# from bank.Login.routes import Login
-# from bank.Customer.routes import Customer
-# from bank.Employee.routes import Employee
-# app.register_blueprint(Login)
-# app.register_blueprint(Customer)
-# app.register_blueprint(Employee)
-  
-#webbrowser.open("http://127.0.0.1:5000", new=0, autoraise=True)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
